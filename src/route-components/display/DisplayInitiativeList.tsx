67caee844af233ee6f1a3046938cecef7177319c
Dan Alexander <lxndrdagreat@gmail.com>
2021-02-08 20:18:48 -0500

    basic initiative list for Display

--- src/route-components/display/DisplayInitiativeList.tsx
+++ src/route-components/display/DisplayInitiativeList.tsx
@@ -1,37 +1,30 @@
-import {h} from 'preact';
+import { h } from 'preact';
 import type { CombatCharacterSchema } from '../../schemas/combat-character.schema';
 import type { RootState } from '@store/reducer';
 import { connect } from 'react-redux';
 import './DisplayInitiativeList.css';
 
 interface Props {
-  characters: CombatCharacterSchema[];
+  characters: (CombatCharacterSchema | null)[];
   total: number;
 }
 
 const maxShowing = 10;
 
-function DisplayInitiativeList({characters, total}: Props) {
+function DisplayInitiativeList({ characters, total }: Props) {
   return (
-    <div className='DisplayInitiativeList'>
-      {
-        characters.map(character =>
-          (
-            <div className='character'>
-              { character.displayName }
-            </div>
-          )
-        )
-      }
+    <div className="DisplayInitiativeList">
+      {characters.map((character) => {
+        return character ? (
+          <div className="character">{character.displayName}</div>
+        ) : (
+          <div className="character next-round">Next Round</div>
+        );
+      })}
 
-      {
-        total > maxShowing ? (
-          <div className='character'>
-            +{ total - maxShowing } more
-          </div>
-        ) : null
-      }
-
+      {total > maxShowing ? (
+        <div className="character plus-more">+{total - maxShowing} more</div>
+      ) : null}
     </div>
   );
 }
@@ -40,18 +33,23 @@ function mapStateToProps(state: RootState): Props {
   if (!state.combatTracker) {
     return {
       characters: [],
-      total: 0
-    }
+      total: 0,
+    };
   }
-  const activeCharacterIndex = state.combatTracker.characters.findIndex(ch => ch.id === state.combatTracker!.activeCharacterId);
-  const characters = state.combatTracker.characters
+
+  let characters: (CombatCharacterSchema | null)[] = state.combatTracker.characters
     .slice()
-    .sort((a, b) => b.roll - a.roll)
+    .sort((a, b) => b.roll - a.roll);
+  characters.push(null);
+  const activeCharacterIndex = characters.findIndex(
+    (ch) => ch && ch.id === state.combatTracker!.activeCharacterId,
+  );
+  characters = characters
     .rotate(activeCharacterIndex >= 0 ? activeCharacterIndex : 0)
     .slice(0, maxShowing);
   return {
     characters: characters,
-    total: state.combatTracker.characters.length
+    total: state.combatTracker.characters.length,
   };
 }
 
