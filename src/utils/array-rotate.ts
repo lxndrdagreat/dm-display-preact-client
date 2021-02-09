28f09c57970bd8153d9b75866535c8b143d66dda
Dan Alexander <lxndrdagreat@gmail.com>
2021-02-08 20:00:00 -0500

    fix: bug in array.rotate

--- src/utils/array-rotate.ts
+++ src/utils/array-rotate.ts
@@ -4,5 +4,8 @@ interface Array<T> {
 }
 
 Array.prototype.rotate = function(n) {
+  if (this.length > 0) {
+    n = n % this.length;
+  }
   return this.slice(n, this.length).concat(this.slice(0, n));
 }
